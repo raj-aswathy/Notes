431d0aa3d144290942a0f6e7633f8849566abe20
Vajahath Ahmed <vajuoff.1@gmail.com>
2024-09-03 15:47:51 +0530

    promise-corrections

--- promiseSep3.js
+++ promiseSep3.js
@@ -1,45 +1,46 @@
 function start() {
   console.log("Start");
 }
-function parallelendPoint() {
-    console.log("Hitting parallel responses");
-    return Promise.all([hitEndPoint1(),hitEndPoint2(),hitEndPoint3()]) 
-    .then(response => {
-        console.log(response);
-        console.log("step 2");
-        
-    }) 
+function parallelEndPoint() {
+  console.log("Hitting parallel responses");
+  return Promise.all([hitEndPoint1(), hitEndPoint2(), hitEndPoint3()]);
 }
 
 async function hitEndPoint1() {
-    return new Promise ((resolve)=>{
-        console.log("Hitting End point 1");
-    })
+  return new Promise((resolve) => {
+    console.log("Hitting End point 1");
+    resolve();
+  });
 }
 async function hitEndPoint2() {
-    return new Promise ((resolve)=>{
-        console.log("Hitting End point 2");
-    })
+  return new Promise((resolve) => {
+    console.log("Hitting End point 2");
+    resolve();
+  });
 }
 async function hitEndPoint3() {
-    return new Promise ((resolve)=>{
-        console.log("Hitting End point 3");
-    })
+  return new Promise((resolve) => {
+    console.log("Hitting End point 3");
+    resolve();
+  });
 }
 async function hitEndPoint4() {
-    return new Promise ((resolve)=>{
-        console.log("Hitting End point 4");
-    })
+  return new Promise((resolve) => {
+    console.log("Hitting End point 4");
+    resolve();
+  });
 }
 async function hitEndPoint5() {
-    return new Promise ((resolve)=>{
-        console.log("Hitting End point 5");
-    })
+  return new Promise((resolve) => {
+    console.log("Hitting End point 5");
+    resolve();
+  });
 }
 async function hitEndPoint6() {
-    return new Promise ((resolve)=>{
-        console.log("Hitting End point 6");
-    })
+  return new Promise((resolve) => {
+    console.log("Hitting End point 6");
+    resolve();
+  });
 }
 async function hitEndPointSequential1() {
   await hitEndPoint1();
@@ -53,26 +54,30 @@ async function hitEndPointSequential3() {
   await hitEndPoint5();
   await hitEndPoint6();
 }
-async function mainTask() {
-    console.log("Hitting sequential responses");
-    return Promise.all([hitEndPointSequential1(),hitEndPointSequential2(),hitEndPointSequential3()]) 
-    .then(response => {
-        console.log("Response",response);        
-    }) 
+
+function mainTask() {
+  console.log("Hitting sequential responses");
+  return Promise.all([
+    hitEndPointSequential1(),
+    hitEndPointSequential2(),
+    hitEndPointSequential3(),
+  ]);
 }
+
 function finish() {
   console.log("Finish");
 }
-async function main() {
-    try{
-        start();
-        await parallelendPoint();
-        await mainTask();
-        finish();
-    }catch(error){
-        console.error("Error",error)
-    }
 
+async function main() {
+  try {
+    start();
+    await parallelEndPoint();
+    console.log("step 2");
+    await mainTask();
+    finish();
+  } catch (error) {
+    console.error("Error", error);
+  }
 }
 
 main();
